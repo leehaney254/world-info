471b3a3030cec5c23e212faa7f0bde6134b24785
Leehaney gee <georgeleehaney20@gmail.com>
2023-02-06 18:54:32 +0300

    Style: Add details page

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -4,7 +4,33 @@ import Navbar from '../components/Navbar';
 const Details = () => (
   <div>
     <Navbar data="Town/City info" />
-    Details
+    <main>
+      <div>
+        image
+        <div>
+          <p>kenya</p>
+          <p>Currency</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>Capital</p>
+          <p>Nairobi</p>
+        </div>
+        <div>
+          <p>Area</p>
+          <p>580367.0</p>
+        </div>
+        <div>
+          <p>Population</p>
+          <p> 53771300</p>
+        </div>
+        <div>
+          <p>timezones</p>
+          <p>UTC+03:00</p>
+        </div>
+      </div>
+    </main>
   </div>
 );
 
