1fe655cae81e9880615e6fd9b7e458463f35eeca
Leehaney gee <georgeleehaney20@gmail.com>
2023-02-07 07:35:30 +0300

    Feat: Display data on home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchCountries } from '../redux/countries/countries';
 import Navbar from '../components/Navbar';
 import Form from '../components/Form';
@@ -10,9 +10,20 @@ import './Home.css';
 
 const Home = () => {
   const dispatch = useDispatch();
+  const countriesData = useSelector((state) => state);
   useEffect(() => {
     dispatch(fetchCountries());
   }, [dispatch]);
+
+  const { countries } = countriesData;
+
+  const displayCountries = countries.map((country) => (
+    <Country
+      key={country.countryName}
+      name={country.countryName}
+      population={country.population}
+    />
+  ));
   return (
     <>
       <Navbar data="Countries" />
@@ -20,7 +31,16 @@ const Home = () => {
       <Form />
       <p>Countries</p>
       <div>
-        <Country data="kenya" />
+        {countriesData.loading && <h1>Loading...</h1>}
+        {!countriesData.loading && countriesData.error ? (
+          <div>
+            Error:
+            {countriesData.error}
+          </div>
+        ) : null}
+        {!countriesData.loading && countriesData.countries.length ? (
+          <div>{displayCountries}</div>
+        ) : null}
       </div>
     </>
   );
